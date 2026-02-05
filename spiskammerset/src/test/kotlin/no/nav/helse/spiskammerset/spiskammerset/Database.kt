c0a86730c177ca7e39f516e98a1dc70c23118846
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2026-02-05 08:23:32 +0100

    Sletter de riktige tabellene 💡

--- spiskammerset/src/test/kotlin/no/nav/helse/spiskammerset/spiskammerset/Database.kt
+++ spiskammerset/src/test/kotlin/no/nav/helse/spiskammerset/spiskammerset/Database.kt
@@ -4,7 +4,7 @@ import com.github.navikt.tbd_libs.test_support.CleanupStrategy
 import com.github.navikt.tbd_libs.test_support.DatabaseContainers
 import javax.sql.DataSource
 
-val databaseContainer = DatabaseContainers.container("spiskammerset", CleanupStrategy.tables("forsikring"))
+val databaseContainer = DatabaseContainers.container("spiskammerset", CleanupStrategy.tables("hendelse, hylle, hendelser_paa_hylla, hylleeier"))
 fun databaseTest(testblokk: (DataSource) -> Unit) {
     val testDataSource = databaseContainer.nyTilkobling()
     try {
